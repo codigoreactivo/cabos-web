08880113cc98a3f74acb0a00d0c2d0ab669df9f7
codigoreactivo <jesusjhoel.aquino@outlook.com>
2023-06-14 12:41:44 -0500

    slider v2

--- components/Cantina.jsx
+++ components/Cantina.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Cantina = () => {
   return (
     <div className="flex flex-row h-screen w-full z-10">
-        <div></div>
-        <div></div>
+        <div className=""></div>
+        <div className=""></div>
     </div>
   );
 };
