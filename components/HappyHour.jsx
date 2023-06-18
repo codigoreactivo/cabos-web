ffc0087384121e82efccb0cf8008f69f3ead29ab
codigoreactivo <jesusjhoel.aquino@outlook.com>
2023-06-17 22:33:23 -0500

    responsive 70%

--- components/HappyHour.jsx
+++ components/HappyHour.jsx
@@ -6,7 +6,7 @@ import "aos/dist/aos.css";
 
 const HappyHour = () => {
   return (
-    <div className="grid h-auto lg:grid-cols-[60%_minmax(0,_1fr)] xl:w-[80%] 2xl:w-[90%] lg:gap-[8rem] gap-14 lg:text-left ">
+    <div className="grid h-auto lg:grid-cols-[60%_minmax(0,_1fr)] w-full xl:w-[80%] 2xl:w-[88%] lg:gap-[8rem] gap-14 lg:text-left ">
       <div className="grid h-fit lg:justify-items-start justify-items-center">
         <h2 className="text-6xl leading-none md:text-7xl lg:text-8xl xl:text-8xl 2xl:text-9xl font-[CreatoDisplay-Black]">
           <Image
@@ -26,32 +26,32 @@ const HappyHour = () => {
           </span>
         </h2>
         <span className="lg:w-[85%]">
-        <p className="w-[clamp] lg:text-[1.75rem] leading-none text-[#525252] font-[CreatoDisplay-Light]">
+        <p className="w-[clamp] text-[1.35rem] lg:text-[1.75rem] pt-7 lg:pt-0 txt-align-lts  lg:text-left text-justify text-xl leading-none text-[#525252] font-[CreatoDisplay-Light]">
           At Los Cabos Mexican Restaurant, we proudly showcase the rich and
           diverse culinary traditions of Mexico, where every dish is crafted
           with passion and an irresistible blend of spices.
         </p>
         </span>
-        <p className="text-[#525252] lg:text-[1.75rem] font-[CreatoDisplay-ThinItalic] my-4">
+        <p className="text-[#525252] text-[1.35rem] lg:text-[1.75rem] font-[CreatoDisplay-ThinItalic] my-4">
           We love what we do!
         </p>
         <Image src="/img/underline-img.png" width={100} height={20} />
       </div>
       <div className="grid h-fit text-[#525252]">
-        <h2 className="lg:text-[2rem] leading-none font-[CreatoDisplay-Black] uppercase text-[#525252]">
+        <h2 className="text-[1.8rem] lg:text-[2rem] leading-none font-[CreatoDisplay-Black] uppercase text-[#525252]">
           Located in Dickinson,
           <br /> North Dakota
         </h2>
-        <h3 className="lg:text-[1.75rem] font-[CreatoDisplay-Bold] leading-none">
+        <h3 className="text-[1.4rem] lg:text-[1.75rem] font-[CreatoDisplay-Bold] leading-none">
           We are open 7 days a week <br /> hours of operation:
         </h3>
-        <ul className="lg:py-12 leading-none">
+        <ul className="text-[1.4rem] py-6 lg:py-12 leading-none">
           <li className="lg:text-[1.75rem] font-[CreatoDisplay-Light]">10:30am- 9:15pm</li>
           <li className=" lg:text-[1.75rem] font-[CreatoDisplay-Medium]">
             Monday-Thursday
           </li>
         </ul>
-        <ul className="lg:py-2 leading-none">
+        <ul className="text-[1.4rem] lg:py-2 leading-none">
         <li className="lg:text-[1.75rem] font-[CreatoDisplay-Light]">10:30am- 9:30pm</li>
           <li className="lg:text-[1.75rem] font-[CreatoDisplay-Medium]">
             Friday and Saturday
@@ -79,8 +79,8 @@ const HappyHour = () => {
           <p className="font-[CreatoDisplay-Black] text-6xl lg:text-7xl text-[#525252]">
             HOUR
           </p>
-          <ul className="leading-none">
-            <li className="lg:text-[1.75rem] font-[CreatoDisplay-Light]">10:30am- 5:00pm</li>
+          <ul className="leading-none py-6 lg:py-0 text-[1.4rem]">
+            <li className=" lg:text-[1.75rem] font-[CreatoDisplay-Light]">10:30am- 5:00pm</li>
             <li className="lg:text-[1.75rem] font-[CreatoDisplay-Medium]">Monday-Friday</li>
           </ul>
         </div>
