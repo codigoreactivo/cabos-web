d1f9115a22253ee18ae5f5002aecd7f7c3fc8beb
codigoreactivo <jesusjhoel.aquino@outlook.com>
2023-06-21 10:02:12 -0500

    responsive all done

--- components/GridSection.jsx
+++ components/GridSection.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import Image from "next/image";
 
 const GridSection = () => {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://snapwidget.com/js/snapwidget.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       <div className="bg-white" >
@@ -11,16 +23,7 @@ const GridSection = () => {
         width={2500}
         height={80}
       ></Image>
-        <iframe
-          src="https://snapwidget.com/embed/1035407"
-          className="snapwidget-widget"
-          style={{
-            border: "none",
-            overflow: "hidden",
-            width: "100%",
-            height: "auto",
-          }}
-        ></iframe>
+        <iframe src="https://snapwidget.com/embed/1036357" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style={{border:'none', overflow:'hidden',  width:'100%',}}></iframe>
       </div>
       <Image
         className=" mt-[-10] top-0 z-10 align-middle rotate-180 mx-auto "
